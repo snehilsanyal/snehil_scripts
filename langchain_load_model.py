a5f1cd8c5dcb4b2c53f85b6845e5130498662f63
snehilsanyal <snehilsanyal@gmail.com>
2023-05-16 15:10:43 +0530

    Update langchain_load_model.py to include LORA model loading

--- langchain_load_model.py
+++ langchain_load_model.py
@@ -1,25 +1,55 @@
 # Script for loading a local model to interact with langchain
+import argparse 
+import os 
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from peft import PeftModel 
 from langchain import PromptTemplate, LLMChain
 from langchain.llms import HuggingFacePipeline
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
+parser = argparse.ArgumentParser(description = 'Load locally hosted model from server\
+                                 for further usage via LangChain')
+parser.add_argument('-m', '--model_name')
+args = parser.parse_args()
 
 template = """Question: {question}
 
-Answer:"""
+Answer:\n"""
 prompt = PromptTemplate(template=template, input_variables=["question"])
 print(prompt)
 
 # Model path (directory folder) contains pytorch.bin/flaxweights/model.safetensors and tokenizer
+llm_dir = '~/models/llm/'
+base_model_path = llm_dir + args.model_name
+is_base = True 
 
-model_path = "facebook_galactica-125m"
-model = AutoModelForCausalLM.from_pretrained(model_path)
-tokenizer = AutoTokenizer.from_pretrained(model_path)
+## PEFT tuners
+# Check whether model is LORA model
+is_lora = is_base and base_model_path.endswith('lora')
+filelist = os.listdir(base_model_path)
+for file in filelist:
+    if is_lora and file.endswith(('adapter_config.json', 'adapter_model.bin')):
+        is_lora = True 
+
+# Load base model
+base_model = AutoModelForCausalLM.from_pretrained(base_model_path, return_dict = True, device_map = 'Auto')
+tokenizer = AutoTokenizer.from_pretrained(base_model_path)
+
+# If model is LORA load peft model
+if is_lora:
+    peft_model = PeftModel.from_pretrained(base_model, peft_model_id)
+    model = peft_model
+    print("=========================================================\
+          Successfully loaded LORA Model: {}".format(args.model_name))
+# Else load base model
+else:
+    model = base_model
+    print("=========================================================\
+          Successfully loaded Base Model: {}".format(args.model_name))
 
 # max_length has typically been deprecated for max_new_tokens 
 pipe = pipeline(
     "text-generation", model=model, tokenizer = tokenizer,
-      max_new_tokens=64, model_kwargs={"temperature":0})
+      max_new_tokens=30, model_kwargs={"temperature":0})
 hf = HuggingFacePipeline(pipeline=pipe)
 
 llm_chain = LLMChain(prompt=prompt, llm=hf)
