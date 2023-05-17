b2b9c39f57db2f6e301fb85a3c33df49a2af05dc
snehilsanyal <snehilsanyal@gmail.com>
2023-05-17 19:15:12 +0530

    Add custom_llm.py, template for CustomLLM Class

---
+++ custom_llm.py
@@ -0,0 +1,25 @@
+## Script to create Custom LLM Class 
+# LangChain Issue, GitHub: https://github.com/hwchase17/langchain/issues/737
+# 1. Try out a structure in the repo in form of a library (so that everything can be imported)
+# 2. Create custom LLM using a locally hosted model and tokenizer
+# 3. Merge 2 LLMs into a single pipeline/CustomLLM/ using this script
+import os, argparse
+import torch
+from langchain.llms.base import LLM
+from transformers import AutoModelForCausalML, AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
+
+class CustomLLM(LLM):
+    model_name = ''
+    tokenizer = ''
+    pipe = pipeline(task = 'text2text-generation', model = model_name,
+                        device = 'cuda:0', tokenizer = tokenizer, 
+                        model_kwargs = {"torch_dtype":torch.bfloat16}) 
+
+    def _call(self, prompt, stop=None):
+        return self.pipeline(prompt, max_length=9999)[0]["generated_text"]
+
+    def _identifying_params(self):
+        return {"name_of_model": self.model_name}
+
+    def _llm_type(self):
+        return "custom"
